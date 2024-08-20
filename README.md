3f8993d6b060cdab2c3a392e8541395ded1f8b43
ymzksgkz <83740740+ymzksgkz@users.noreply.github.com>
2024-08-21 00:26:42 +0900

    とりあえず動くようになった。

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+CDK を使うのが初めての場合、初回に必要なコマンド
+zenn の記事を参考にしたので、初回〜デプロイまでに必要なコマンドは下記参照のこと
+https://zenn.dev/hirokisakabe/articles/73d7d30a0e2ec8
+
+**スタック名**: EcsPracticeStack
+
+テンプレートを作成する時引数に vpcId を渡す必要がある
+```shell
+$ cdk synth -c vpcId=<デプロイする VPC の ID>
+```
+
+デプロイ
+```shell
+$ cdk deploy EcsPracticeStack
+```
+
+お片付け
+```shell
+$ cdk destroy EcsPracticeStack
+```
+
+以下 CDK デフォルトの文言
+
+--- 
+
 # Welcome to your CDK TypeScript project
 
 This is a blank project for CDK development with TypeScript.
--- lib/ecs-practice-stack.ts
+++ lib/ecs-practice-stack.ts
@@ -1,16 +1,65 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
-export class EcsPracticeStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class EcsPracticeStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, {
+      env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+      },
+      ...props,
+    });
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsPracticeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpcId = this.node.tryGetContext("vpcId");
+    const vpc = ec2.Vpc.fromLookup(this, "DefaultVPC", { vpcId: vpcId });
+    const cluster = new ecs.Cluster(this, "Cluster", { vpc });
+
+    // VPCエンドポイントを追加
+    new ec2.InterfaceVpcEndpoint(this, "EcrApiEndpoint", {
+      vpc,
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+    });
+
+    new ec2.InterfaceVpcEndpoint(this, "EcrDkrEndpoint", {
+      vpc,
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+    });
+
+    new ec2.GatewayVpcEndpoint(this, "S3Endpoint", {
+      vpc,
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+    });
+
+    new ec2.InterfaceVpcEndpoint(this, 'CloudWatchLogsEndpoint', {
+      vpc,
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+    });
+
+    const repository = ecr.Repository.fromRepositoryName(
+      this,
+      "EcsPractice",
+      "ecs-practice"
+    );
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "FargateService",
+      {
+        cluster,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromEcrRepository(repository),
+        },
+        runtimePlatform: {
+          cpuArchitecture: ecs.CpuArchitecture.ARM64
+        },
+        publicLoadBalancer: true,
+      }
+    );
   }
 }
